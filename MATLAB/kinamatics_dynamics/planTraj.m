3ff366db4e5871e2135007845cf96e3bc60b8f9b
ngoldfar <ngoldfar@stevens.edu>
2016-12-01 21:07:33 -0500

    The sign on the coef was flipped.This was corrected

--- MATLAB/kinamatics_dynamics/planTraj.m
+++ MATLAB/kinamatics_dynamics/planTraj.m
@@ -9,9 +9,9 @@ t = time;
 %get the coefs for theta
 a_t  = q_i;
 
-c_t = 3*(q_i - q_f)/ (t^2);
+c_t = 3*(q_f - q_i)/ (t^2);
 
-d_t = 2*( q_f - q_i)/(t^3);
+d_t = -2*( q_f - q_i)/(t^3);
 
 traj = [ a_t,0,c_t,d_t];
 
